ce114954205bd3c35f6bf237c9f1401bcaa3f40b
valayashrajredandwhite-cloud <valayashrajredandwhite@gmail.com>
2026-03-16 11:20:54 +0530

    'A'

--- script.js
+++ script.js
@@ -3,20 +3,26 @@ const rows = 30;
 const cols = 20;
 
 const boxes = [];
-// const snake = [{ x: 1, y: 3 }];
+const snake = [{ x: 1, y: 3 }];
 
 for (let row = 0; row < rows; row++) {
     for (let col = 0; col < cols; col++) {
         const box = document.createElement('div');
         box.classList.add('box');
         board.appendChild(box);
-        // box.innerText = `${row}-${col}`;
+        box.innerText = `${row}-${col}`;
         boxes[`${row}-${col}`] = box;
     }
 }
 
 function render() {
     snake.forEach(segment => {
-        boxes[`${segment.x}-${segment.y}`].classList.add[`snake`]
+        boxes[`${segment.x}-${segment.y}`].classList.add["snake"]
+    })
+}
+
+function render() {
+    snake.forEach(segment => {
+        boxes[`${segment.x}-${segment.y}`].classList.add["snake"]
     })
 }
